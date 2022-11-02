979562d4eebf0b6e0fc18a7f672b24164a9e873d
xiongbinbin.xbb <xiongbinbin.xbb@alibaba-inc.com>
2022-11-02 11:38:06 +0800

    update

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
+	"log"
 	"net/http"
 	"sync"
 
@@ -62,7 +62,7 @@ func (v *Room) Remove(p *Participant) {
 	}
 }
 
-func (v *Room) Notify(ctx context.Context, peer *Participant, event, data string) {
+func (v *Room) Forward(ctx context.Context, peer *Participant, data []byte) {
 	var participants []*Participant
 
 	for _, r := range participants {
@@ -70,41 +70,119 @@ func (v *Room) Notify(ctx context.Context, peer *Participant, event, data string
 			continue
 		}
 
-		res := struct {
-			Action string `json:"action"`
-			Event  string `json:"event"`
-			Data   string `json:"data,omitempty"`
-		}{
-			"notify", event, data,
-		}
-
-		b, err := json.Marshal(struct {
-			Message interface{} `json:"msg"`
-		}{
-			res,
-		})
-		if err != nil {
-			return
-		}
-
 		select {
 		case <-ctx.Done():
 			return
-		case r.Out <- b:
+		case r.Out <- data:
 		}
-
-		fmt.Println("Notify %v about %v %v", r, peer, event)
 	}
 }
 
 func main() {
 	ctx := context.Background()
+	var rooms sync.Map
 
-	http.Handle("/sig", websocket.Handler(func(c *websocket.Conn) {
+	http.Handle("/sig/v1/p2p", websocket.Handler(func(c *websocket.Conn) {
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
 
-		r := c.Request()
+		req := c.Request()
+		log.Printf("client %v at %v", req.RemoteAddr, req.RequestURI)
 		defer c.Close()
+
+		var self *Participant
+		go func() {
+			<-ctx.Done()
+			if self == nil {
+				return
+			}
+
+			self.Room.Remove(self)
+			log.Printf("Remove client %v", self)
+		}()
+
+		inMessages := make(chan []byte, 0)
+		go func() {
+			defer cancel()
+
+			buf := make([]byte, 16384)
+			for {
+				n, err := c.Read(buf)
+				if err != nil {
+					break
+				}
+
+				select {
+				case <-ctx.Done():
+				case inMessages <- buf[:n]:
+				}
+			}
+		}()
+
+		outMessages := make(chan []byte, 0)
+		go func() {
+			defer cancel()
+
+			handleMessage := func(m []byte) error {
+				obj := struct {
+					TID     string `json:"tid"`
+					Message struct {
+						Action  string `json:"action"`
+						Room    string `json:"room"`
+						Display string `json:"display"`
+					} `json:"msg"`
+				}{}
+
+				if err := json.Unmarshal(m, &obj); err != nil {
+					return errors.New("message format error")
+				}
+
+				r, _ := rooms.LoadOrStore(obj.Message.Room, &Room{Name: obj.Message.Room})
+				var p *Participant
+
+				if obj.Message.Action == "join" {
+					p = &Participant{Room: r.(*Room), Display: obj.Message.Display, Out: outMessages}
+					if err := r.(*Room).Add(p); err != nil {
+						return errors.New("Add Participant Error")
+					}
+
+					self = p
+					log.Printf("Join %v ok", self)
+				} else if obj.Message.Action == "leave" {
+					p = r.(*Room).Get(obj.Message.Display)
+					if p == nil {
+						return errors.New("Get Participant Error")
+					}
+
+					r.(*Room).Remove(p)
+					log.Printf("Leave %v ok", p)
+				} else {
+					p = r.(*Room).Get(obj.Message.Display)
+					if p == nil {
+						return errors.New("Get Participant Error")
+					}
+				}
+
+				go r.(*Room).Forward(ctx, p, m)
+
+				return nil
+			}
+
+			for m := range inMessages {
+				if err := handleMessage(m); err != nil {
+					log.Printf("Handle %s err %v", m, err)
+					break
+				}
+			}
+		}()
+
+		for m := range outMessages {
+			if _, err := c.Write(m); err != nil {
+				log.Printf("Websocket Write error %v for %v", err, req.RemoteAddr)
+				break
+			}
+		}
 	}))
+
+	http.ListenAndServe(":1988", nil)
 }
